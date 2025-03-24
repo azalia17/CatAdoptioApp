1d982a04e5aaa1a0b2ba14d438c82bd6edd60aff
Azalia <azaliaaps17@gmail.com>
2025-03-24 08:43:25 +0700

    Initial Commit

---
+++ CatAdoptio/CatAdoptioApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatAdoptioApp.swift
+//  CatAdoptio
+//
+//  Created by Azalia Amanda on 24/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CatAdoptioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatAdoptioTests/CatAdoptioTests.swift
@@ -0,0 +1,17 @@
+//
+//  CatAdoptioTests.swift
+//  CatAdoptioTests
+//
+//  Created by Azalia Amanda on 24/03/25.
+//
+
+import Testing
+@testable import CatAdoptio
+
+struct CatAdoptioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
